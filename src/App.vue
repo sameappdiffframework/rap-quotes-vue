e3e743edc6bb716656391a542f346173bc573da1
Colby M. White <colbywhite@users.noreply.github.com>
2021-09-12 15:13:41 -0500

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <Header></Header>
+  <Header/>
+  <Footer/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Header from './components/Header.vue';
 import './App.css';
+import Header from './components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    Header
-  }
+  components: { Footer, Header }
 });
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer>
+    <ul class="no-bullets">
+      <li>
+        <a href="https://github.com/sameappdiffframework" target="_blank">Same App, Different Framework</a>
+      </li>
+      <li>
+        <a href="https://github.com/sameappdiffframework/rap-quotes-angular" target="_blank">Source code</a>
+      </li>
+    </ul>
+    <p>
+      This site is made by
+      <a href="https://github.com/colbywhite" target="_blank">Colby M. White</a>
+      and hosted for free by Netlify.
+      It is a part of the
+      <a href="https://github.com/sameappdiffframework" target="_blank">Same App, Different Framework</a>
+      project.
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+footer {
+  font-size: .7em;
+  display: flex;
+  justify-content: space-between;
+  margin: 1em 0 0 0;
+  border-top: 1px solid var(--primary-accent);
+  padding: 1em 0 0 0;
+  gap: 1em;
+}
+
+footer p {
+  flex-basis: 50%;
+  margin: 0;
+}
+</style>
